7768361b989cf653b5138df465beb4e51f791911
zac <doicirohangnan@gmail.com>
2021-02-13 14:11:40 -0500

    Save state 1

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,7 +1,12 @@
 package ui;
+import model.*;
 
 public class Main {
   public static void main(String[] args) {
-
+    Genome huntingtons = new Genome();
+    System.out.println(huntingtons.getEveryCodon().toString());
+    System.out.println("-------------");
+    System.out.println("Head: " + huntingtons.getHead());
+    System.out.println("Tail: " + huntingtons.getTail());
   }
 }
